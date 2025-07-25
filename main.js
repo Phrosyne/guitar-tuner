4c7c03b8ccc86b7204d055b545a6f658583bcee7
Phrosyne <mcoderej@gmail.com>
2025-07-24 20:09:11 -0700

    Add files via upload
    Added classic guitar background colors and fonts for the page. Imported favicon and audio plus adding buttons to actually trigger each string. Next step is to figure out button color plus make it prettier in general. Maybe fade out the audio too if I can figure that out.

---
+++ main.js
@@ -0,0 +1,29 @@
+function clickLowE() {
+    let music = new Audio("/audio/6th-string.m4a");
+    music.play();
+}
+
+function clickA() {
+    let music = new Audio("/audio/5th-string.m4a");
+    music.play();
+}
+
+function clickD() {
+    let music = new Audio("/audio/4th-string.m4a");
+    music.play();
+}
+
+function clickG() {
+    let music = new Audio("/audio/3rd-string.m4a");
+    music.play();
+}
+
+function clickB() {
+    let music = new Audio("/audio/2nd-string.m4a");
+    music.play();
+}
+
+function clickHighE() {
+    let music = new Audio("/audio/1st-string.m4a");
+    music.play();
+}
