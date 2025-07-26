e67aa314758877af88fa45d2d589ef2dedcef077
Phrosyne <mcoderej@gmail.com>
2025-07-25 21:15:35 -0700

    Add files via upload
    I had originally created individual functions for each button press like the infamous isEven experiment but saw how it was just really inefficient. I tried to make a map, storing the audio as the value and the id as the key pair but I was struggling with Object Reference Errors. Taking a break from going through all the documentation, I decided to instead start with a switch statement and it worked pretty well.

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-function clickLowE() {
-    let music = new Audio("/audio/6th-string.m4a");
-    music.play();
-}
+const low_e_string = new Audio("/audio/6th-string.m4a");
+const a_string = new Audio("/audio/5th-string.m4a");
+const d_string = new Audio("/audio/4th-string.m4a");
+const g_string = new Audio("/audio/3rd-string.m4a");
+const b_string = new Audio("/audio/2nd-string.m4a");
+const high_e_string = new Audio("/audio/1st-string.m4a");
 
-function clickA() {
-    let music = new Audio("/audio/5th-string.m4a");
-    music.play();
-}
+function clickBtn(element) {
+    let e_id = element.id;
+    switch (e_id) {
+        case '6':
+            low_e_string.play();
+            break;
+        case '5':
+            a_string.play();
+            break;
+        case '4':
+            d_string.play();
+            break;
+        case '3':
+            g_string.play();
+            break;
+        case '2':
+            b_string.play();
+            break;
+        case'1':
+            high_e_string.play();
+            break;
 
-function clickD() {
-    let music = new Audio("/audio/4th-string.m4a");
-    music.play();
-}
-
-function clickG() {
-    let music = new Audio("/audio/3rd-string.m4a");
-    music.play();
-}
-
-function clickB() {
-    let music = new Audio("/audio/2nd-string.m4a");
-    music.play();
-}
-
-function clickHighE() {
-    let music = new Audio("/audio/1st-string.m4a");
-    music.play();
+        default:
+            alert("Sorry but it looks like nothing happened :(");
+    }
 }
