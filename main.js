1ffded2d54e5a13ba0f1fb668aaf8098d63338ef
Phrosyne <mcoderej@gmail.com>
2025-07-25 21:38:28 -0700

    Add files via upload
    Apparently only the local server was receiving the actual sound from the buttons, while the github website didn't get anything. It should be fixed now, but I'll only really know after committing...

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const low_e_string = new Audio("/audio/6th-string.m4a");
-const a_string = new Audio("/audio/5th-string.m4a");
-const d_string = new Audio("/audio/4th-string.m4a");
-const g_string = new Audio("/audio/3rd-string.m4a");
-const b_string = new Audio("/audio/2nd-string.m4a");
-const high_e_string = new Audio("/audio/1st-string.m4a");
+// const low_e_string = new Audio("/audio/6th-string.m4a");
+// const a_string = new Audio("/audio/5th-string.m4a");
+// const d_string = new Audio("/audio/4th-string.m4a");
+// const g_string = new Audio("/audio/3rd-string.m4a");
+// const b_string = new Audio("/audio/2nd-string.m4a");
+// const high_e_string = new Audio("/audio/1st-string.m4a");
+
+const low_e_string = document.getElementById("sixth-string");
+const a_string = document.getElementById("fifth-string");
+const d_string = document.getElementById("fourth-string");
+const g_string = document.getElementById("third-string");
+const b_string = document.getElementById("second-string");
+const high_e_string = document.getElementById("first-string");
+
 
 function clickBtn(element) {
-    let e_id = element.id;
-    switch (e_id) {
+    switch (element.id) {
         case "six":
             low_e_string.play();
             break;
